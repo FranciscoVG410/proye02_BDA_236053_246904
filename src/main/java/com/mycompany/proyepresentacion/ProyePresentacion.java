699627207388eb9141d69c0b80d8042c8de11ec8
franciscoVG410 <francisco.valdez246904@potros.itson.edu.mx>
2024-03-17 17:59:00 -0700

    simon

---
+++ src/main/java/com/mycompany/proyepresentacion/ProyePresentacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyepresentacion;
+
+/**
+ *
+ * @author franc
+ */
+public class ProyePresentacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
