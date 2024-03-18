c97b1a033cb3b1217b8c9f9392208892c4c673c1
franciscoVG410 <francisco.valdez246904@potros.itson.edu.mx>
2024-03-17 18:52:38 -0700

    Persistencia

---
+++ src/main/java/com/mycompany/proyepersistencia/ProyePersistencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyepersistencia;
+
+/**
+ *
+ * @author franc
+ */
+public class ProyePersistencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
